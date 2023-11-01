778e330f64f589e918f64208be25bf1665e3b6ce
Anna Kontsevaya <anna.kontsevaya@paragon-software.com>
2023-11-01 17:12:49 +0300

    added lab1

---
+++ src/main/java/ru/polytech/labs/lab1/books/BooksMain.java
@@ -0,0 +1,55 @@
+package ru.polytech.labs.lab1.books;
+
+import ru.polytech.labs.lab1.books.model.Book;
+import ru.polytech.labs.lab1.books.model.Publisher;
+
+public class BooksMain {
+    public static void main(String[] args) {
+        Publisher[] publishers = new Publisher[]{
+                new Publisher("Проспект", "Москва"),
+                new Publisher("Питер", "Санкт-Петербург"),
+                new Publisher("БХВ", "Санкт-Петебург"), // Ошибка сделана намеренно
+                new Publisher("Диалектика", "Киев")
+        };
+
+        Book[] books = new Book[]{
+                new Book(
+                        "Computer Science: основы программирования на Java, ООП, алгоритмы и структуры данных",
+                        new String[]{"Седжвик Роберт", "Уэйн Кевин"},
+                        2018,
+                        publishers[1]
+                ),
+                new Book(
+                        "Разработка требований к программному обеспечению. 3-е издание, дополненное",
+                        new String[]{"Вигерс Карл"},
+                        2019,
+                        publishers[2]
+                ),
+                new Book(
+                        "Java. Полное руководство, 10-е издание",
+                        new String[]{"Шилдт Герберт"},
+                        2018,
+                        publishers[3]
+                ),
+                new Book(
+                        "C/C++. Процедурное программирование",
+                        new String[]{"Полубенцева М.И."},
+                        2017,
+                        publishers[2]
+                ),
+                new Book("Конституция РФ", 2020, publishers[0])
+        };
+
+        System.out.println("Исходный массив:");
+        Book.printAll(books);
+
+        // Исправляем ошибку в названии города издательства "БХВ"
+        publishers[2].setCity("Санкт-Петербург");
+
+        System.out.println("Массив после исправления названия города:");
+        Book.printAll(books);
+
+        // После исправления в название города издательства "БХВ" и повторной печати, вывод изменится для всех книг,
+        // этого издательства, так как замена значения города была произведена у ссылки
+    }
+}
